6e43b9f18a02b829b98aac113733cce53e4d2de6
yaroslavLarionov <yarillo@yahoo.com>
2022-04-10 21:14:31 -0400

    modified version

--- src/utilities/collaborator_1.java
+++ src/utilities/collaborator_1.java
@@ -1,9 +1,15 @@
-package utilities;
 
 public class collaborator_1 {
-
-    // write a main method to reverse String
-    // JAVA => AVAJ
-
+	public static void main(String[] args) {
+		
+		String str = "JAVA";
+		String revStr = "";
+		
+		for (int i = str.length() - 1; i >= 0; i--) {
+			revStr += str.charAt(i);
+		}
+		System.out.println("Original string is: " + str);
+		System.out.println("Reversed string is: " + revStr);
+	}
 
 }
--- src/utilities/collaborator_2.java
+++ src/utilities/collaborator_2.java
@@ -1,9 +1,27 @@
-package utilities;
 
 public class collaborator_2 {
+	public static void main(String[] args) {
+		
+		fib(3); // -> 0 1 1
+		fib(5); // -> 0 1 1 2 3
+		fib(6); // -> 0 1 1 2 3 5
+		fib(10); // -> 0 1 1 2 3 5 8 13 21 34
+		
+		
+	}
 
-    // Write a main method to print fibonacci series
-    // example: 1 2 3 5 8 13...
+	public static void fib(int n) {
+		int firstNumber = 0;
+		int secondNumber = 1;
 
+		for (int i = 0; i < n; i++) {
+			System.out.print(firstNumber + " ");
+			int sum = firstNumber + secondNumber;
+			firstNumber = secondNumber;
+			secondNumber = sum;
+
+		}
+		System.out.println();
+	}
 
 }
